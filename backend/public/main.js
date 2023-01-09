8b1b90bd372152b8f281a9b5b7020df55eb75eac
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2023-01-09 01:43:15 -0600

    routing fixes

--- backend/public/main.js
+++ backend/public/main.js
@@ -281,7 +281,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/api/v1/',
+    apiUrl: '/',
 };
 /*
  * For easier debugging in development mode, you can import the following file
